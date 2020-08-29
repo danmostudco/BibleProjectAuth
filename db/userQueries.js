59bf97eac077bf9c3505dc36ffad5bd84b272870
Dan Morrison <morrison.danr@gmail.com>
2020-08-29 14:26:21 -0400

    create users in PG database, and also login users by checking hashed password

--- db/userQueries.js
+++ db/userQueries.js
@@ -1,4 +1,5 @@
 const pool = require("./pool");
+const bcrypt = require("bcrypt");
 
 const getUsers = (request, response) => {
   pool.query("SELECT * FROM users", (error, results) => {
@@ -20,7 +21,53 @@ const getUserById = (request, response) => {
   });
 };
 
+const createUser = async (request, response) => {
+  const { name, password } = request.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const accountName = name;
+    console.log(hashedPassword);
+    pool.query(
+      "INSERT INTO users (name, password) VALUES ($1, $2) RETURNING *",
+      [accountName, hashedPassword],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        response.status(201).send(`User added with ID: ${results.rows[0].id}`);
+      }
+    );
+  } catch {
+    response.status(500).send();
+  }
+};
+
+const login = async (request, response) => {
+  const { name, password } = request.body;
+  const { rows } = await pool.query("SELECT * FROM users WHERE name = $1", [
+    name,
+  ]);
+  user = rows[0];
+
+  if (user == null) {
+    return response.status(400).send("cannot find user");
+  }
+
+  try {
+    if (await bcrypt.compare(password, user.password)) {
+      response.status(201).send("Success");
+    } else {
+      response.status(401).send("not allowed");
+    }
+  } catch {
+    response.status(500).send();
+  }
+};
+
 module.exports = {
   getUsers,
   getUserById,
+  createUser,
+  login,
 };
--- server.js
+++ server.js
@@ -6,39 +6,9 @@ const userQueries = require("./db/userQueries.js");
 
 app.use(express.json());
 
-// this will become a DB eventually
-// walt pw = dogs, kent password = cats
-const users = [];
-
 app.get("/users", userQueries.getUsers);
 app.get("/users/:id", userQueries.getUserById);
-
-app.post("/register", async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    console.log(hashedPassword);
-    const user = { name: req.body.name, password: hashedPassword };
-    users.push(user);
-    res.status(201).send();
-  } catch {
-    res.status(500).send();
-  }
-});
-
-app.post("/users/login", async (req, res) => {
-  const user = users.find((user) => user.name === req.body.name);
-  if (users == null) {
-    return res.status(400).send("cannot find user");
-  }
-  try {
-    if (await bcrypt.compare(req.body.password, user.password)) {
-      res.send("Success");
-    } else {
-      res.send("not allowed");
-    }
-  } catch {
-    res.status(500).send();
-  }
-});
+app.post("/create", userQueries.createUser);
+app.post("/users/login", userQueries.login);
 
 app.listen(3000);
